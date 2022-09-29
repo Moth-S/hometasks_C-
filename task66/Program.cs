5dd5dd3880a9c01786983a87fa12ee1d26fbe3d9
Lyubov Si <gse182@yandex.ru>
2022-09-29 23:19:06 +0300

    add task 66

---
+++ task66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int GetIntNumber() ///возвращает целое случайное число
+{
+    return new Random().Next(1, 20);
+}
+
+int SumNumbers(int startNum, int endNum)
+{
+    if (startNum == endNum) return startNum;
+
+    return startNum + SumNumbers(startNum + 1, endNum);
+}
+
+int startNum = GetIntNumber();
+int endNum = GetIntNumber();
+
+if (startNum > endNum)
+{
+    Console.WriteLine("Так не работает! :Р");
+    return;
+}
+
+Console.WriteLine($"Сумма чисел от {startNum} до {endNum}: " + SumNumbers(startNum, endNum));
